0d61974f05bef948cbcfa591f6434c4280a9b0e9
MarianneB08 <mboyer.correction.relecture@gmail.com>
2023-03-25 19:57:29 +0100

    Configuration des styles généraux du site : création du fichier globals.module.scss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./Router";
+import styles from "./globals.module.scss";
 
 // Composant global qui contient l'ensemble des routes de l'application et le layout général (Header & Footer) défini dans Layout.js
 function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <div className={styles.main}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </div>
   );
 }
 
