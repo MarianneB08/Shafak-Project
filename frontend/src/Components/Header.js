6593f7fdccfecab05e2a4991efeb2b02e50b15fc
MarianneB08 <mboyer.correction.relecture@gmail.com>
2023-03-26 17:18:14 +0200

    Mise en place du composant Header.js et de la navigation

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './Header.module.scss';
 
 const Header = () => {
     return (
-        <div>
-            Hi from Header !
-        </div>
+        <header className={styles.container}>
+            <h1>Compagnie Shafak</h1>
+            <nav className={styles.nav}>
+                <ul className={styles.navItems}>
+                    <li><NavLink to="/">Accueil</NavLink></li>
+                    <li><NavLink to="/company">La compagnie</NavLink></li>
+                    <li><NavLink to="/creations">Créations</NavLink></li>
+                    <li><NavLink to="/agenda">Agenda</NavLink></li>
+                    <li><NavLink to="/partners">Partenaires</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
+                </ul>
+            </nav>
+        </header>
     );
 };
 
