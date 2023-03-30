122d2c91398ee38136a5a71b96778e832e5a60da
MarianneB08 <mboyer.correction.relecture@gmail.com>
2023-03-30 21:23:57 +0200

    Début de mise en place du burger menu dans le header sur les résolutions inférieures à 480px

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,37 +1,48 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./Header.module.scss";
+import { useState } from "react";
 
 const Header = () => {
+
+  const [showLinks, setShowLinks] = useState(false);
+
+  const handleShowLinks = () => {
+    setShowLinks(!showLinks)
+  }
+
   return (
     <header className={styles.container}>
       <h1>Compagnie Shafak</h1>
-      <nav className={styles.nav}>
-        <ul className={styles.navItems}>
-          <li>
+      <nav className={`${styles.navbar} ${showLinks ? styles.showNavbar : ""}` }>
+        <ul className={styles.navbarLinks}>
+          <li className={styles.navbarItem}>
             <NavLink to="/">Accueil</NavLink>
           </li>
 
-          <li>
+          <li className={styles.navbarItem}>
             <NavLink to="/company">La compagnie</NavLink>
           </li>
 
-          <li>
+          <li className={styles.navbarItem}>
             <NavLink to="/creations">Créations</NavLink>
           </li>
 
-          <li>
+          <li className={styles.navbarItem}>
             <NavLink to="/agenda">Agenda</NavLink>
           </li>
 
-          <li>
+          <li className={styles.navbarItem}>
             <NavLink to="/partners">Partenaires</NavLink>
           </li>
 
-          <li>
+          <li className={styles.navbarItem}>
             <NavLink to="/contact">Contact</NavLink>
           </li>
         </ul>
+        <button className={styles.navBurger} onClick={handleShowLinks}>
+          <span className={styles.burgerLine}></span>
+        </button>
       </nav>
     </header>
   );
