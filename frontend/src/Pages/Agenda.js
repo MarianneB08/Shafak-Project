59ba5971ad409d39a378ef4631c2edd8abbd71b2
MarianneB08 <mboyer.correction.relecture@gmail.com>
2023-03-30 18:00:14 +0200

    Ajout du contenu de la page Agenda.js

--- frontend/src/Pages/Agenda.js
+++ frontend/src/Pages/Agenda.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
+import styles from "./Agenda.module.scss";
 
 const Agenda = () => {
-    return (
-        <div>
-            Hello from Agenda !
-        </div>
-    );
+  return (
+    <main className={styles.container}>
+      <h1>Agenda</h1>
+      <div className={styles.datesList}>
+        <section className={styles.date}>
+          <p>
+            <span className={styles.monthAndYear}>avril 2023</span> – <span className={styles.title}>Le loup</span> – Festival Rabi-Al-Jasad, Tunis,
+            Tunisie
+          </p>
+        </section>
+        <section className={styles.date}>
+          <p>
+            <span className={styles.monthAndYear}>avril 2023</span> – <span className={styles.title}>Le loup</span> – 24h-du-théâtre-non-stop, Kef,
+            Tunisie
+          </p>
+        </section>
+        <section className={styles.date}>
+          <p>
+            <span className={styles.monthAndYear}>juin 2023</span> – <span className={styles.title}>Le loup</span> – Festival de danse de Carthage,
+            Tunis, Tunisie
+          </p>
+        </section>
+        <section className={styles.date}>
+          <p>
+            <span className={styles.monthAndYear}>juillet 2023</span> – <span className={styles.title}>Point de vue</span> – Lieux dévoilés
+            prochainement
+          </p>
+        </section>
+      </div>
+    </main>
+  );
 };
 
 export default Agenda;
