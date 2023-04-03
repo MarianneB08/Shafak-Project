9be79b22b50c1a6b9cc5b1498f4df77d0c3af5de
MarianneB08 <mboyer.correction.relecture@gmail.com>
2023-04-03 16:59:16 +0200

    Ajout d'un muter pour le son de la vidéo de la page d'accueil dans le composant BackgroundVideo.js

--- frontend/src/Components/BackgroundVideo.js
+++ frontend/src/Components/BackgroundVideo.js
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import video from "../assets/background_video.mp4";
 import styles from "./BackgroundVideo.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeXmark, faVolumeHigh } from "@fortawesome/free-solid-svg-icons";
 
 const BackgroundVideo = () => {
+  const [isMute, setIsMute] = useState(false);
+
+  const muteHandle = () => {
+    setIsMute((isMute) => !isMute);
+  };
+  console.log(isMute);
+
   return (
     <section className={styles.container}>
-      <video src={video} autoPlay loop muted />
+      {isMute ? (
+        <video src={video} autoPlay loop />
+      ) : (
+        <video src={video} autoPlay loop muted />
+      )}
+      {isMute ? (
+        <FontAwesomeIcon
+          icon={faVolumeHigh}
+          onClick={muteHandle}
+          className={styles.muteIcon}
+        />
+      ) : (
+        <FontAwesomeIcon
+          icon={faVolumeXmark}
+          onClick={muteHandle}
+          className={styles.muteIcon}
+        />
+      )}
+
       <p className={styles.text}>
         Compagnie de danse contemporaine et arts performatifs ayant pour but la
-        production, l’exploration, la promotion et la diffusion des créations des
-        artistes chorégraphiques Larbi Namouchi & Margot Libanga.
+        production, l’exploration, la promotion et la diffusion des créations
+        des chorégraphes Larbi Namouchi & Margot Libanga.
       </p>
     </section>
   );
