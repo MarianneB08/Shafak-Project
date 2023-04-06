9a6b820f911ca67f92d78d78e1d6fc4c4b3ef87f
MarianneB08 <mboyer.correction.relecture@gmail.com>
2023-04-06 20:11:32 +0200

    Ajout des contenus relatifs à la modale des mentions légales dans le fichier fr.json

--- frontend/src/Components/Popup.js
+++ frontend/src/Components/Popup.js
@@ -11,112 +11,70 @@ import rtlStyles from "./Popup_rtl.module.scss";
 
 const Popup = ({ trigger, setTrigger }) => {
   // Utilisation de la feuille de style RTL ou LTR en fonction de la langue sélectionnée par l'utilisateur
-  const { userLanguage } = useContext(LanguageContext);
+  const { dictionary, userLanguage } = useContext(LanguageContext);
   let styles = userLanguage === "ar" ? rtlStyles : ltrStyles;
 
   return trigger ? (
     <div className={styles.popupContainer}>
-      <h3 className={styles.popupTitle}>Mentions légales</h3>
+      <h3 className={styles.popupTitle}>{dictionary.legalNotice.popupTitle}</h3>
       <div className={styles.popupText}>
         <div className={styles.popupTextIdentity}>
-          <h4>Identité</h4>
+          <h4>{dictionary.legalNotice.popupIdentity.identityTitle}</h4>
           <ul>
-            <li>Nom du site web : Compagnie Shafak</li>
-            <li>Adresse : </li>
-            <li>Propriétaire : Compagnie Shafak</li>
-            <li>Responsable de publication : Compagnie Shafak</li>
-            <li>Conception et réalisation : Marianne Boyer</li>
-            <li>Hébergement : Netlify</li>
+            <li>{dictionary.legalNotice.popupIdentity.siteName}</li>
+            <li>{dictionary.legalNotice.popupIdentity.siteUrl}</li>
+            <li>{dictionary.legalNotice.popupIdentity.siteOwner}</li>
+            <li>{dictionary.legalNotice.popupIdentity.siteEditor}</li>
+            <li>{dictionary.legalNotice.popupIdentity.siteDesigner}</li>
+            <li>{dictionary.legalNotice.popupIdentity.siteHosting}</li>
           </ul>
         </div>
         <div className={styles.popupTextConditions}>
-          <h4>Conditions d'utilisation</h4>
-          <p>
-            L’utilisation du présent site implique l’acceptation pleine et
-            entière des conditions générales d’utilisation décrites ci-après.
-            Ces conditions d’utilisation sont susceptibles d’être modifiées ou
-            complétées à tout moment.
-          </p>
+          <h4>{dictionary.legalNotice.popupConditions.conditionsTitle}</h4>
+          <p>{dictionary.legalNotice.popupConditions.conditionsText}</p>
         </div>
         <div className={styles.popupTextInformations}>
-          <h4>Informations</h4>
-          <p>
-            Les informations et documents du site sont présentés à titre
-            indicatif, sans de caractère exhaustif, et ne peuvent engager la
-            responsabilité du propriétaire du site. Le propriétaire du site ne
-            peut être tenu responsable des dommages directs et indirects
-            consécutifs à l’accès au site.
-          </p>
+          <h4>{dictionary.legalNotice.popupInformations.informationsTitle}</h4>
+          <p>{dictionary.legalNotice.popupInformations.informationsText}</p>
         </div>
         <div className={styles.popupTextProperty}>
-          <h4>Propriété intellectuelle</h4>
-          <p>
-            Sauf mention contraire, tous les éléments accessibles sur le site
-            (textes, images, graphismes, logo, icônes, sons, logiciels, etc.)
-            restent la propriété exclusive de leurs auteurs, en ce qui concerne
-            les droits de propriété intellectuelle ou les droits d’usage.
-          </p>
+          <h4>{dictionary.legalNotice.popupProperty.propertyTitle}</h4>
+          <p>{dictionary.legalNotice.popupProperty.propertyParagraph}</p>
           <ol>
             <li>
-              Toute reproduction, représentation, modification, publication,
-              adaptation de tout ou partie des éléments du site, quel que soit
-              le moyen ou le procédé utilisé, est interdite, sauf autorisation
-              écrite préalable de l’auteur.
+              {dictionary.legalNotice.popupProperty.propertyFirstListItem}
             </li>
             <li>
-              Toute exploitation non autorisée du site ou de l’un quelconque des
-              éléments qu’il contient est considérée comme constitutive d’une
-              contrefaçon et passible de poursuites.
+              {dictionary.legalNotice.popupProperty.propertySecondListItem}
             </li>
             <li>
-              Les marques et logos reproduits sur le site sont déposés par les
-              sociétés qui en sont propriétaires.
+              {dictionary.legalNotice.popupProperty.propertyThirdListItem}
             </li>
           </ol>
         </div>
         <div className={styles.popupTextLinks}>
-          <h4>Liens</h4>
-          <p className={styles.outLinks}>Liens sortants</p>
-          <p>
-            Le propriétaire du site décline toute responsabilité et n’est pas
-            engagé par le référencement via des liens hypertextes, de ressources
-            tierces présentes sur le réseau Internet, tant en ce qui concerne
-            leur contenu que leur pertinence.
+          <h4>{dictionary.legalNotice.popupLinks.linksTitle}</h4>
+          <p className={styles.outLinks}>
+            {dictionary.legalNotice.popupLinks.outLinksTitle}
           </p>
-          <p className={styles.inLinks}>Liens entrants</p>
-          <p>
-            Le propriétaire du site autorise les liens hypertextes vers l’une
-            des pages de ce site, à condition que ceux-ci ouvrent une nouvelle
-            fenêtre et soient présentés de manière non équivoque afin d’éviter :
-            tout risque de confusion entre le site citant et le propriétaire du
-            site ainsi que toute présentation tendancieuse, ou contraire aux
-            lois en vigueur. Le propriétaire du site se réserve le droit de
-            demander la suppression d’un lien s’il estime que le site source ne
-            respecte pas les règles ainsi définies.
+          <p>{dictionary.legalNotice.popupLinks.outLinksText}</p>
+          <p className={styles.inLinks}>
+            {dictionary.legalNotice.popupLinks.inLinksTitle}
           </p>
+          <p>{dictionary.legalNotice.popupLinks.inLinksText}</p>
         </div>
         <div className={styles.popupTextConfidentiality}>
-          <h4>Confidentialité</h4>
-          <p>
-            Tout utilisateur dispose d’un droit d’accès, de rectification et
-            d’opposition aux données personnelles le concernant, en effectuant
-            sa demande écrite et signée, accompagnée d’une preuve d’identité. Le
-            site ne recueille pas d’informations personnelles, et n’est pas
-            assujetti à déclaration à la CNIL.
-          </p>
+          <h4>{dictionary.legalNotice.popupPrivacy.privacyTitle}</h4>
+          <p>{dictionary.legalNotice.popupPrivacy.privacyText}</p>
         </div>
         <div className={styles.popupTextCredits}>
-          <h4>Crédits</h4>
+          <h4>{dictionary.legalNotice.popupCredits.creditsTitle}</h4>
           <p>
-            Les auteurs des photographies utilisées pour illustrer ce site web
-            sont mentionnés au survol de chaque photographie.
+            {dictionary.legalNotice.popupCredits.creditsText}
             <br />
-            Crédits vidéo page d'accueil : Compagnie Shafak
+            {dictionary.legalNotice.popupCredits.creditsComplement}
           </p>
-          <p>
-            Les icônes utilisées sur ce site sont chargées depuis le site
-            https://fontawesome.com.
-          </p>
+          <p>{dictionary.legalNotice.popupCredits.creditsIcons}</p>
         </div>
         {/* La fermeture de la modale au clic sur le bouton "Fermer" est gérée par le setter du hook useState passé en props au composant "Popup"
         depuis le composant parent "Footer". */}
@@ -124,7 +82,7 @@ const Popup = ({ trigger, setTrigger }) => {
           className={styles.closeButton}
           onClick={() => setTrigger(false)}
         >
-          Fermer
+          {dictionary.legalNotice.closeButton}
         </button>
       </div>
     </div>
