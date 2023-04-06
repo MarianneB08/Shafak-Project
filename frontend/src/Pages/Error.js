27486d63ca91bb177f932e1ac8b280c8aa8bd4a1
MarianneB08 <mboyer.correction.relecture@gmail.com>
2023-04-06 11:24:39 +0200

    Modifications sur la page Error.js : ajout d'une image de fond et ajustements sur la structure JSX de la page

--- frontend/src/Pages/Error.js
+++ frontend/src/Pages/Error.js
@@ -4,6 +4,7 @@ import { useContext } from "react";
 import { LanguageContext } from "../store/languageContext.js";
 import ltrStyles from "./Error_ltr.module.scss";
 import rtlStyles from "./Error_rtl.module.scss";
+import backgroundImg from "../assets/error_background.jpg";
 
 const Error = () => {
   // Utilisation de la feuille de style RTL ou LTR en fonction de la langue sélectionnée par l'utilisateur
@@ -12,11 +13,18 @@ const Error = () => {
 
   return (
     <main className={styles.container}>
-      <p className={styles.errorType}>{dictionary.error.errorType}</p>
-      <p className={styles.errorDescription}>{dictionary.error.errorDescription}</p>
-      <Link to="/">
-        <p className={styles.backHome}>{dictionary.error.homeLink}</p>
-      </Link>
+      <img src={backgroundImg} alt="Larbi Namouchi by Garance Li" />
+      <section className={styles.text}>
+        <p className={styles.errorType}>{dictionary.error.errorType}</p>
+        <div className={styles.details}>
+          <p className={styles.errorDescription}>
+            {dictionary.error.errorDescription}
+          </p>
+          <Link to="/">
+            <p className={styles.backHome}>{dictionary.error.homeLink}</p>
+          </Link>
+        </div>
+      </section>
     </main>
   );
 };
