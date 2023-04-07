96fe98c2878b60e6def91c215e4a21b08e8239f9
MarianneB08 <mboyer.correction.relecture@gmail.com>
2023-04-07 19:38:30 +0200

    Modifications sur la page Partners.js : structure JSX et modifications des logos des partenaires

--- frontend/src/Pages/Partners.js
+++ frontend/src/Pages/Partners.js
@@ -3,42 +3,58 @@ import { useContext } from "react";
 import { LanguageContext } from "../store/languageContext.js";
 import ltrStyles from "./Partners_ltr.module.scss";
 import rtlStyles from "./Partners_rtl.module.scss";
-import logo1 from "../assets/1.png";
-import logo2 from "../assets/2.png";
-import logo3 from "../assets/3.png";
-import logo4 from "../assets/4.png";
-import logo5 from "../assets/5.png";
-import logo6 from "../assets/6.png";
-import logo7 from "../assets/7.png";
-import logo8 from "../assets/8.png";
-import logo9 from "../assets/9.png";
-import logo10 from "../assets/10.png";
-import logo11 from "../assets/11.png";
-import logo12 from "../assets/12.png";
-import logo13 from "../assets/13.png";
+import logo1 from "../assets/logo_region_bretagne.png";
+import logo2 from "../assets/logo_binic-etable-sur-mer.png";
+import logo3 from "../assets/logo_pimoff.png";
+import logo4 from "../assets/logo_DRAC_auvergne_rhone-alpes.png";
+import logo5 from "../assets/logo_le_croiseur.png";
+import logo6 from "../assets/logo_dampfzentrale_bern.png";
+import logo7 from "../assets/logo_cnd.png";
+import logo8 from "../assets/logo_institut_francais_tanger.png";
+import logo9 from "../assets/logo_plerin-sur-mer.png";
+import logo10 from "../assets/logo_desoblique.png";
+import logo11 from "../assets/logo_la_machinerie_venissieux.png";
+import logo12 from "../assets/logo_dansometre.png";
+import logo13 from "../assets/logo_tregueux.png";
+import logo14 from "../assets/logo_lo_studio.png";
+import logo15 from "../assets/logo_cite_culture_tunis.png";
+import logo16 from "../assets/logo_centre_arts_dramatiques_sceniques_kef.png";
 
 const Partners = () => {
   // Utilisation de la feuille de style RTL ou LTR en fonction de la langue sélectionnée par l'utilisateur
   const { dictionary, userLanguage } = useContext(LanguageContext);
   let styles = userLanguage === "ar" ? rtlStyles : ltrStyles;
 
+  const logosArray = [
+    logo1,
+    logo2,
+    logo3,
+    logo4,
+    logo5,
+    logo6,
+    logo7,
+    logo8,
+    logo9,
+    logo10,
+    logo11,
+    logo12,
+    logo13,
+    logo14,
+    logo15,
+    logo16,
+  ];
+
   return (
     <main className={styles.container}>
       <h1>{dictionary.partnersPage.firstTitle}</h1>
       <section className={styles.logos}>
-        <img src={logo1} alt="" />
-        <img src={logo2} alt="" />
-        <img src={logo3} alt="" />
-        <img src={logo4} alt="" />
-        <img src={logo5} alt="" />
-        <img src={logo6} alt="" />
-        <img src={logo7} alt="" className={styles.logo7} />
-        <img src={logo8} alt="" className={styles.logo8} />
-        <img src={logo9} alt="" />
-        <img src={logo10} alt="" />
-        <img src={logo11} alt="" />
-        <img src={logo12} alt="" />
-        <img src={logo13} alt="" />
+        {logosArray.map((logo) => {
+          return (
+            <div className={styles.logoContainer} key={logo}>
+              <img src={logo} alt="" className={styles.logo} />
+            </div>
+          );
+        })}
       </section>
     </main>
   );
