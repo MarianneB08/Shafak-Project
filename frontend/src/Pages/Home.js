070080651b2e3caa36c7693eed88392a816fb53b
MarianneB08 <mboyer.correction.relecture@gmail.com>
2023-04-08 19:51:25 +0200

    Réorganisation des fichiers .scss des pages/composants : BackgroundVideo.js, Footer.js, Header.js, LanguageSelector.js, Popup.js, Agenda.js, Archives.js et Company.js

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,4 +1,5 @@
-import React from 'react';
+// La page Home.js correspond à la page d'accueil du site. Elle n'est constituée que du composant BackgroundVideo.js.
+
 import BackgroundVideo from '../components/BackgroundVideo';
 
 const Home = () => {
