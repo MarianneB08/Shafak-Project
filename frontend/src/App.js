1731d72c790022ceca51727a12e6e6867b164054
MarianneB08 <mboyer.correction.relecture@gmail.com>
2023-04-26 09:38:04 +0200

    Rectifications de styles suite à la mise en production du site

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import styles from "./globals.module.scss";
 import Router from "./Router";
 import Layout from "./components/Layout";
 import { LanguageProvider } from "./store/languageContext";
+// import Preloader from "./components/Preloader";
 
 // Composant global qui contient l'ensemble des routes de l'application et le layout général (Header & Footer) défini dans Layout.js.
 // Le LanguageProvider englobe l'ensemble de l'application pour fournir l'ensemble des pages en contenus français et anglais.
